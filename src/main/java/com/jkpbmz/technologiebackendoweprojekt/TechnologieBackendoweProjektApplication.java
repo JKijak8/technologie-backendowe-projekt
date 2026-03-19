a6a0f082ce0eab02f2832285dca2834d9ca1ff64
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2026-03-19 01:34:59 +0100

    feat: add dependencies.

---
+++ src/main/java/com/jkpbmz/technologiebackendoweprojekt/TechnologieBackendoweProjektApplication.java
@@ -0,0 +1,13 @@
+package com.jkpbmz.technologiebackendoweprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologieBackendoweProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnologieBackendoweProjektApplication.class, args);
+    }
+
+}
