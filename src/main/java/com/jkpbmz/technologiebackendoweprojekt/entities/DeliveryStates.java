00bc05e5883464f70aca27b51e953841c4e1ced9
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2026-03-20 18:12:59 +0100

    feat: add DeliveryStates entity.

---
+++ src/main/java/com/jkpbmz/technologiebackendoweprojekt/entities/DeliveryStates.java
@@ -0,0 +1,38 @@
+package com.jkpbmz.technologiebackendoweprojekt.entities;
+
+import com.jkpbmz.technologiebackendoweprojekt.enums.DeliveryStatesEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnTransformer;
+
+import java.time.ZonedDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "delivery_states")
+public class DeliveryStates {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "delivery_state")
+    @Enumerated(EnumType.STRING)
+    @ColumnTransformer(write = "?::STATES")
+    private DeliveryStatesEnum deliveryState;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "last_updated", columnDefinition = "TIMESTAMP WITH TIMEZONE")
+    private ZonedDateTime lastUpdated;
+}
---
+++ src/main/java/com/jkpbmz/technologiebackendoweprojekt/enums/DeliveryStatesEnum.java
@@ -0,0 +1,14 @@
+package com.jkpbmz.technologiebackendoweprojekt.enums;
+
+public enum DeliveryStatesEnum {
+    PENDING,
+    ACCEPTED,
+    IN_WAREHOUSE,
+    ASSIGNED,
+    IN_TRANSIT,
+    OUT_FOR_DELIVERY,
+    DELIVERED,
+    FAILED_ATTEMPT,
+    RETURNED,
+    CANCELLED
+}
