405d931c901419c2bca4b79a78e2cb64b782860d
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2026-03-20 17:58:04 +0100

    feat: add Contract entity.

---
+++ src/main/java/com/jkpbmz/technologiebackendoweprojekt/entities/Contract.java
@@ -0,0 +1,26 @@
+package com.jkpbmz.technologiebackendoweprojekt.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contracts")
+public class Contract {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "sender_address")
+    private String senderAddress;
+}
