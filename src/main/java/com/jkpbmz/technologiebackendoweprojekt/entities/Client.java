6aa8918f8b048d2616390dab9c801cc441444fd3
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2026-03-20 17:54:13 +0100

    feat: add Client entity.

---
+++ src/main/java/com/jkpbmz/technologiebackendoweprojekt/entities/Client.java
@@ -0,0 +1,32 @@
+package com.jkpbmz.technologiebackendoweprojekt.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "nip")
+    private String nip;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "email")
+    private String email;
+}
