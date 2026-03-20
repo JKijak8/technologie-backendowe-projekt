70ea9f2786758cac267f6c54f42ed5bc109d8399
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2026-03-20 17:50:33 +0100

    feat: create Driver entity.

---
+++ src/main/java/com/jkpbmz/technologiebackendoweprojekt/entities/Driver.java
@@ -0,0 +1,29 @@
+package com.jkpbmz.technologiebackendoweprojekt.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "drivers")
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+}
